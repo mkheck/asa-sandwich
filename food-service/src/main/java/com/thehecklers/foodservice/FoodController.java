10d5666262fc8ec339d0769ef1ded65a1a181747
Mark Heckler <mark.heckler@gmail.com>
2023-09-14 14:59:32 -0500

    Food

--- food-service/src/main/java/com/thehecklers/foodservice/FoodController.java
+++ food-service/src/main/java/com/thehecklers/foodservice/FoodController.java
@@ -1,9 +1,11 @@
 package com.thehecklers.foodservice;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
-import java.util.Collections;
+import java.util.UUID;
 
 @RestController
 public class FoodController {
@@ -15,6 +17,14 @@ public class FoodController {
 
     @GetMapping("/random")
     public Food getRandomFood() {
-        return new Food("1", new Starch("1", "Potato", 1), Collections.singletonList(new Topping("1", "Bacon", Taste.SAVORY)));
+        //var starchClient = RestClient.create("http://localhost:8082/random");
+        //var toppingClient = RestClient.create("http://localhost:8083/random");
+        var starchClient = RestClient.create("http://starch-service/random");
+        var toppingClient = RestClient.create("http://topping-service/random");
+
+        var starch = starchClient.get().retrieve().body(Starch.class);
+        var toppings = toppingClient.get().retrieve().body(new ParameterizedTypeReference<Iterable<Topping>>() {});
+
+        return new Food(UUID.randomUUID().toString(), starch, toppings);
     }
 }
